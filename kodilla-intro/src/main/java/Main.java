d64c1b5b50b7dd85ed3edc6b7a1ad4db0f64181f
Urszula Kapica <urszula.kapica@gmail.com>
2020-09-06 13:47:15 +0200

    add new files

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myAge);
+        System.out.println(myName);
+        System.out.println(height);
+        System.out.println(isGoodProgrammer);
+
+        String Welcome = "Hello from First Class!";
+        System.out.println(Welcome);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,21 @@
+public class MainExtended {
+    public static void main(String[]args){
+        String myDoughterName="Cela";
+        int celaAge=6;
+        boolean isShePupil=true;
+        char celaClass='A';
+        boolean publicSchool=false;
+        int amountOfpupilsInClass=15;
+
+        System.out.println(myDoughterName);
+        System.out.println(celaAge);
+        System.out.println(celaClass);
+        System.out.println(isShePupil);
+        System.out.println(publicSchool);
+        System.out.println(amountOfpupilsInClass);
+
+
+
+    }
+
+}
