0d5ec0070cd80816b0615aac1e0ac63bc631e026
Urszula Kapica <urszula.kapica@gmail.com>
2020-09-07 14:59:34 +0200

    add files

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+
+    }
+
+
+}
---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,29 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+
+        String[]series=new String[]{"Wikings","Witcher","Mr Robot","Chernobyl","Outlander"};
+
+        String[] series1=new String[5];
+
+        series1[0]="Wikings";
+        series1[1]="Witcher";
+        series1[2]="Mr Robot";
+        series1[3]="Chernobyl";
+        series1[4]="Outlander";
+
+        String serieses=series1[3];
+        System.out.println(series[3]);
+        System.out.println(serieses);
+
+        int numberOfElements=series.length;
+
+
+        System.out.println("Moja tablica zawiera "+ numberOfElements+" pozycji");
+
+
+
+
+
+
+    }
+}
