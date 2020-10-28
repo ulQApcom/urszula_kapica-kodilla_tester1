62e247d461bfe0119a276af92f4c12c5a90fafc3
Urszula Kapica <urszula.kapica@gmail.com>
2020-10-28 21:21:55 +0100

    add random

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -4,6 +4,10 @@ public class RandomNumbers {
     int min = 0;
     int max = 30;
 
+    public static int getRandom(int min, int max){
+
+    }
+
     public static void main(String[]args){
 
         Random random = new Random();
