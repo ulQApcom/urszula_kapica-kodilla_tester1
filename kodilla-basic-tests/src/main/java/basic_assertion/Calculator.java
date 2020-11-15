50a58f810712d84e1f2ac2eb0603d1e3e932f108
Urszula Kapica <urszula.kapica@gmail.com>
2020-11-15 21:20:21 +0100

    Task3.5 Calculator

---
+++ kodilla-basic-tests/src/main/java/basic_assertion/Calculator.java
@@ -0,0 +1,18 @@
+package basic_assertion;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int subtract( int a, int b){
+        return a - b;
+    }
+
+    public int square(int a){
+        return  a * a;
+    }
+}
