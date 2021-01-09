363b1fc795fa0aac19e9b3de80ed8c4829fb95ca
Urszula Kapica <urszula.kapica@gmail.com>
2021-01-09 15:06:58 +0100

    StringManipulator

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/StringManipulator.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/StringManipulator.java
@@ -6,4 +6,14 @@ public class StringManipulator {
         StringBuilder builder = new StringBuilder(input);
         return builder.reverse().toString().toLowerCase();
     }
+    public int getStringLengthWithoutSpaces(String input){
+        String value = input.replaceAll(" ","");
+        return value.length();
+    }
+
+    public int countNumberOffCommas(String text){
+        int count = text.length() - text.replace(",","").length();
+        return count;
+    }
+
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -14,4 +14,18 @@ public class StringManipulatorTestSuite {
     public void shouldReverseStringWithLowerCase(String input, String expected){
         assertEquals(expected, manipulator.reverseWithLowerCase(input));
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test,4", "OtHEr,5","E V e n t,5"," null,4", "A,1"})
+    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected){
+        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"te,st:1","...OtHEr :0","E,V,e,n.t:3", "null :0","A:0"},delimiter = ':')
+    public void shouldCountNumberOfCommas(String input, int expected){
+        assertEquals(expected,manipulator.countNumberOffCommas(input));
+
+
+    }
 }
