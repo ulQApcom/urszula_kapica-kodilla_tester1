e0b67004a5e3b2fa45d56ace1f2af8371258cfeb
Urszula Kapica <urszula.kapica@gmail.com>
2021-02-15 19:04:07 +0100

    Rest Api Task 10.3

---
+++ kodilla-selenium1/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -0,0 +1,7 @@
+package com.kodilla.selenium.google;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ kodilla-selenium3/src/Rest.java
@@ -0,0 +1,5 @@
+public class Rest {
+    public static void main(String[] args) {
+
+    }
+}
